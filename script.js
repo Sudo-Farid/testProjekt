3519dde0c4bd9b6185f64dcb1ee0c542f5319c59
Farid <farid@Air-von-Farid.fritz.box>
2024-10-21 18:53:25 +0200

    JSON filter and findinex funciton

--- script.js
+++ script.js
@@ -1,7 +1,35 @@
-let myObjekt = {
-    'name':'Farid',
-    'Age': '25',
-    'isAgoodGuy': true 
-};
+let myObjectArr = [
+    {
+        "name":"Max",
+        "is_a_good_guy": true 
+    },
+    {
+        "name":"Peter",
+        "is_a_good_guy": false 
+    },
+    {
+        "name":"Arnold",
+        "is_a_good_guy": true 
+    },
+    {
+        "name":"Justus",
+        "is_a_good_guy": true 
+    },
+    {
+        "name":"Bombur",
+        "is_a_good_guy": false 
+    },
+    {
+        "name":"Farid",
+        "is_a_good_guy":true
+    }
+];
 
-console.log(myObjekt);
+let output = myObjectArr.filter(x => x.is_a_good_guy);
+
+
+
+console.log(output);
+console.table(output);
+
+console.log(myObjectArr.findIndex(x => x.name=="Justus"));
